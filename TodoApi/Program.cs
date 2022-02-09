3796a4e0eedfab07ef7500a3e76fa65b03d450dc
Wakahisa <zerakyo@gmail.com>
2022-02-09 14:33:20 -0600

    Add project files.

---
+++ TodoApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+//// learn more about configuring swagger/openapi at https://aka.ms/aspnetcore/swashbuckle
+//builder.services.addendpointsapiexplorer();
+//builder.services.addswaggergen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
